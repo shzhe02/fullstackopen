adbc5521f262ea35fb0d4ba853c30abb84262405
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-08 11:33:03 +0200

    messing around

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -1,7 +1,10 @@
-const Hello = (props) => {
+const Hello = ({name, age}) => {
+  
+  const bornYear = () => new Date().getFullYear() - age
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      <p>Hello {name}, you are {age} years old.</p>
+      <p>So, you were probably born in {bornYear()}.</p>
     </div>
   )
 }
