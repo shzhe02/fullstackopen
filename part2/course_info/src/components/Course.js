9e51f0b69d1eee1f25e7fe260ca4cf0bc988e66e
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-08 19:23:42 +0200

    2.1: Attempted

---
+++ part2/course_info/src/components/Course.js
@@ -0,0 +1,38 @@
+const Header = (props) => {
+    // console.log(props)
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+}
+
+const Content = ({parts}) => {
+    return (
+        parts.map(e => <li key={e.id}><Part part={e.name} exercises={e.exercises} /></li>)
+    )
+}
+
+const Total = (props) => {
+    let sum = 0
+    props.parts.map(e => e.exercises).forEach(e => sum += e)
+    return (
+        <p>Number of exercises {sum}</p>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
