781295b86cccca4fbd4ee3220bb6eb6fd9ea3bf7
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-08 14:22:30 +0200

    1.7: Attempted

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,15 +2,29 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.handler}>{props.label}</button>
 
+const Average = (props) => (props.good - props.bad) / props.total
+
+const Positive = (props) => (props.good / props.total) * 100
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const addGood = () => setGood(good + 1)
-  const addNeutral = () => setNeutral(neutral + 1)
-  const addBad = () => setBad(bad + 1)
+  const addGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+  const addBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
@@ -22,7 +36,10 @@ const App = () => {
       <p>
         good {good}<br />
         neutral {neutral}<br />
-        bad {bad}
+        bad {bad}<br />
+        all {total}<br />
+        average <Average good={good} bad={bad} total={total} /><br />
+        positive <Positive good={good} bad={bad} total={total} />%
       </p>
     </div>
   )
