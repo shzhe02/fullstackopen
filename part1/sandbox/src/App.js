63621f24c54806c242cf9339aa0a25284ddb96d8
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-08 11:37:39 +0200

    messing around

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -1,24 +1,8 @@
-const Hello = ({name, age}) => {
-  
-  const bornYear = () => new Date().getFullYear() - age
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old.</p>
-      <p>So, you were probably born in {bornYear()}.</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name= "Kiera"
-  const age = 25
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Aether" age={15+32} />
-      <Hello name={name} age={age}/>
-    </>
-  )
-}
-
-export default App;
+export default App
