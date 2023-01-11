886c3d3665972591c0da11a1ac22622ce67ae35e
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-11 18:27:24 +0200

    Completed sandbox for part 2

--- part1/sandbox/README.md
+++ part1/sandbox/README.md
@@ -1,3 +1,3 @@
-# Sandbox for Part !
+# Sandbox for Part 1
 
 This file contains code where I am either following along with the course material (non-exercises) and messing around to gain further understanding.
--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -1,15 +1,53 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...') 
+  const [showAll, setShowAll] = useState(true)
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
   
-    return (
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          {notes.map(note => <Note note={note}/>)}
-        </ul>
-      </div>
-    )
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
-  
-  export default App
+
+  const notesToShow = showAll
+    ? notes 
+    : notes.filter(note => note.important === true)
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+export default App
