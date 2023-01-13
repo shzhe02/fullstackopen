43e256561fa0f0b7dd9a4d6ec8e858915a8a82a1
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-13 08:09:15 +0200

    2.12: Attempted

--- part2/country_data/src/App.js
+++ part2/country_data/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CountryList = ({ searchkey, countryList }) => {
+  const filtered = countryList.filter((e) =>
+    e.name.common.toLowerCase().includes(searchkey.toLowerCase())
+  )
+  let list = <></>
+  if (searchkey === "") {
+  } else if (filtered.length > 10) {
+    list = <>Too many matches, specify another filter</>
+  } else if (filtered.length === 1) {
+    const country = filtered[0]
+    list = (
+      <>
+        <h2>{country.name.common}</h2>
+        capital {country.capital} <br />
+        area {country.area} <br />
+        <h4>languages</h4>
+        <ul>
+          {Object.values(country.languages).map((e) => (
+            <li key={e}>{e}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} alt="Flag" />
+      </>
+    )
+  } else {
+    list = filtered.map((e) => <li key={e.name.common}>{e.name.common}</li>)
+  }
+  return list
 }
 
-export default App;
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data)
+    })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      find countries
+      <input onChange={handleSearch} value={search} /> <br />
+      <CountryList countryList={countries} searchkey={search} />
+    </div>
+  )
+}
+
+export default App
