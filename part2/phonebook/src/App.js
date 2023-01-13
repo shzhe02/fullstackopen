ef350b0c663a2f8eb46242f1e2d45b171fd355cc
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-13 07:26:55 +0200

    2.11: Attempted

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react"
 
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import Filter from './components/Filter'
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import Filter from "./components/Filter"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchkey, setSearchkey] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [searchkey, setSearchkey] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addEntry = (event) => {
     event.preventDefault()
     const nameObject = {
-        name: newName,
-        number: newNumber
+      name: newName,
+      number: newNumber,
     }
     const alreadyExists = persons
-      .map(e => e.name === newName)
+      .map((e) => e.name === newName)
       .reduce((collect, b) => collect || b)
     if (!alreadyExists) {
       setPersons(persons.concat(nameObject))
@@ -46,9 +48,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-        <Filter searchkey={searchkey} handleSearchkey={handleSearchkey}/>
+      <Filter searchkey={searchkey} handleSearchkey={handleSearchkey} />
       <h3>Add a new</h3>
-      <PersonForm newName={newName}
+      <PersonForm
+        newName={newName}
         newNumber={newNumber}
         addEntry={addEntry}
         handleName={handleName}
