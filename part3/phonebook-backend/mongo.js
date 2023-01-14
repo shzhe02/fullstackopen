b14de31fa238a89e3f22d3be3505badb2223c2c2
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-14 22:52:25 +0200

    3.12: Attempted

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", true)
+const password = process.argv[2]
+
+const url = `mongodb+srv://fstack:${password}@cluster0.3anbqly.mongodb.net/?retryWrites=true&w=majority`
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Entry = mongoose.model("Entry", entrySchema)
+
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+  mongoose
+    .connect(url)
+    .then(() => {
+      const note = new Entry({
+        name,
+        number,
+      })
+
+      return note.save()
+    })
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+} else if (process.argv.length === 3) {
+  mongoose.connect(url).then(() => {
+    Entry.find({}).then((entries) => {
+      console.log("phonebook:")
+      entries.forEach((entry) => console.log(`${entry.name} ${entry.number}`))
+      return mongoose.connection.close()
+    })
+  })
+} else {
+  console.log("Invalid number of arguments")
+  process.exit(1)
+}
