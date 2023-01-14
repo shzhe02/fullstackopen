93dc4fb9a73ea503fab9e80f63e87f6e52526336
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-14 15:29:10 +0200

    3.5: Attempted

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -54,6 +54,26 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end()
 })
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "no content",
+    })
+  }
+
+  const entry = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number,
+  }
+
+  data = data.concat(entry)
+
+  response.json(entry)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
