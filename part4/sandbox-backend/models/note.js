4fee6c88a3835bf15bc054aaaa64990e2f72df15
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-22 03:57:43 +0200

    4.1: Attempted

--- part4/sandbox-backend/models/note.js
+++ part4/sandbox-backend/models/note.js
@@ -1,19 +1,15 @@
 const mongoose = require("mongoose")
-mongoose.set("strictQuery", true)
 
-const url = process.env.MONGODB_URI
-console.log("connecting to", url)
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to MongoDB")
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message)
-  })
 const noteSchema = new mongoose.Schema({
-  content: { type: String, minLength: 5, required: true },
-  date: { type: Date, required: true },
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 })
 
