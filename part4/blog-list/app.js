b4a4820f32453bb0c08de6455a67b00d90aae27a
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-24 22:15:42 +0200

    4.2: Attempted

---
+++ part4/blog-list/app.js
@@ -0,0 +1,16 @@
+const config = require("./utils/config")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const mongoose = require("mongoose")
+
+//const mongoUrl = "mongodb://localhost/bloglist"
+mongoose.connect(process.env.MONGO_URL)
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/blogs", blogsRouter)
+
+module.exports = app
