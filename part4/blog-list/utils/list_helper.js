73f604d7615a7faeb17f4fb4325869a2fccd2828
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-25 21:27:37 +0200

    4.4: Attempted

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,29 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.map((e) => e.likes).reduce((a, b) => a + b, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length <= 0) {
+    return {}
+  }
+  const best = blogs.reduce(
+    (best, curr) => (curr.likes > best.likes ? curr : best),
+    blogs[0]
+  )
+  const output = {
+    title: best.title,
+    author: best.author,
+    likes: best.likes,
+  }
+  return output
 }
 
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
