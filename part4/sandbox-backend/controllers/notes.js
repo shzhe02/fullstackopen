193a621202983fc6d49d6cdc86517cb7bddda0ec
rv <13660218+shzhe02@users.noreply.github.com>
2023-01-26 18:17:30 +0200

    4.15: Attempted

--- part4/sandbox-backend/controllers/notes.js
+++ part4/sandbox-backend/controllers/notes.js
@@ -2,7 +2,8 @@ const notesRouter = require("express").Router()
 const Note = require("../models/note")
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 })
+
   response.json(notes)
 })
 
@@ -15,17 +16,34 @@ notesRouter.get("/:id", async (request, response) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "")
+  }
+  return null
+}
+
 notesRouter.post("/", async (request, response) => {
   const body = request.body
-
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" })
+  }
+  const user = await User.findById(decodedToken.id)
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user._id,
   })
 
   const savedNote = await note.save()
-  response.status(201).json(savedNote)
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
+
 notesRouter.delete("/:id", async (request, response) => {
   await Note.findByIdAndRemove(request.params.id)
   response.status(204).end()
